77c84e7e975db22f88658f315c5a229f883bcf58
Erik Veland <erik.veland@news.com.au>
2026-09-24 22:56:14 +1000

    Cursor Tests 2.1

--- Scripts/run-playfield-draw-tests.sh
+++ Scripts/run-playfield-draw-tests.sh
@@ -19,6 +19,9 @@ swiftc -swift-version 6 -warnings-as-errors \
   "$project_dir/Sources/LemmingsLocal/SkillCursorBadge.swift" \
   "$project_dir/Sources/LemmingsLocal/ControllerPointer.swift" \
   "$project_dir/Sources/LemmingsLocal/LemmingFocusHighlight.swift" \
+  "$project_dir/Sources/LemmingsLocal/LemmingAssignmentPulse.swift" \
+  "$project_dir/Sources/LemmingsLocal/FailureMood.swift" \
+  "$project_dir/Sources/LemmingsLocal/RewindTransportCue.swift" \
   "$project_dir/Sources/LemmingsLocal/GameplayPresentation.swift" \
   "$project_dir/Sources/LemmingsLocal/PlayfieldView.swift" \
   "$project_dir/Sources/LemmingsLocal/ExplosionHDR.swift" \
